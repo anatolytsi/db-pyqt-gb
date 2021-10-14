9fbb6bf7e0d76ff7a58b0e634df2387902507421
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-10-14 18:25:38 +0200

    lesson8: changed working directories

--- app/client/client.py
+++ app/client/client.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
         f'имя пользователя: {client_name}')
 
     # Загружаем ключи с файла, если же файла нет, то генерируем новую пару.
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.getcwd()
     key_file = os.path.join(dir_path, f'{client_name}.key')
     if not os.path.exists(key_file):
         keys = RSA.generate(2048, os.urandom)
